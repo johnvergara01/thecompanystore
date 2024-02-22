82d7ffd18d358004eb2d2b7b94d9d8ff488d0ade
johnvergara01 <john.vergara01@gmail.com>
2024-02-21 23:02:45 -0800

    Co-authored-by: mel-u <mel-u@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+
+import React from "react";
+import "./App.css";
+import "./Pages/Home.js";
+import { Route, Router, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+    </Router>
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <div>
+    //       <p>heyyyyyyyy</p>
+    //     </div>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
---
+++ src/Pages/Home.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Home() {
+  return <div>test test 1 2 3</div>;
+}
